b2a60e32a680235b81d17f25548956590f0a10d3
00day0 <therandomuser11@gmail.com>
2019-12-26 22:16:06 +0100

    fwb: OOS styled dismiss all button [1/2]
    This is a SQUASH of following commits :-
    
    - OxygenOS styled dismiss all button [1/2]
    
    showing animation might glitch if previous hiding animation was canceled before it finished and set visibility of button to View.GONE
    thats the reason why hiding animation is so short
    
    Before someone will run saying its kang of @imjyotiraditya solution
    I actually didn't know it was already done by him so i created my own solution
    we both looked at oxygenos code to see how it was made so thats why our code is simillar
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    
    - OxygenOS dismiss all: disable animations and disable it by default
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    
    - OxygenOS dismiss all: animate it
    todo: cleanup of all this mess
    
    instead of all this mess with scaling animation lets just switch opacity based on how much panel is expanded
    animation gets in sync with future qs blur animation + it doesnt rely on these damnn checks if panel is fully expanded etc etc etc
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    
    - Enable oxygenos dismiss all by default
    i sorted out problems with animations almost completly so why not enable it by default
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    
    - Fix oxygenos dismiss all appearing when there arent any notifications to dimiss
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: Id303df90f032235b074cd63d4877509006ce3079
    
    - Add ripple effect to oos dismiss all
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: I17099c48e835d18ba87bf48e454a8e5e6f75921e
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1287,7 +1287,7 @@ public class NotificationPanelView extends PanelView implements
         return !mQsTouchAboveFalsingThreshold;
     }
 
-    private float getQsExpansionFraction() {
+    public float getQsExpansionFraction() {
         return Math.min(1f, (mQsExpansionHeight - mQsMinExpansionHeight)
                 / (mQsMaxExpansionHeight - mQsMinExpansionHeight));
     }
@@ -2331,6 +2331,7 @@ public class NotificationPanelView extends PanelView implements
         }
         mNotificationStackScroller.setAlpha(alpha);
         mStatusBar.updateBlurVisibility();
+        mStatusBar.setDismissAllVisible(true);
     }
 
     private float getFadeoutAlpha() {

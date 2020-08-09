38862becbbb518ef3731b7aa2b47ec752e8ff153
Mayur <ultramayur123@gmail.com>
2020-08-09 15:50:16 +0000

    Revert "SystemUI: Make sure volume expand icon points correctly"
    This reverts commit a6c23be801e555ee60bad10194d1573642478a7b.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -341,9 +341,6 @@ public class VolumeDialogImpl implements VolumeDialog,
         mSettingsIcon = mDialog.findViewById(R.id.settings);
         mSettingsIcon.setOnLongClickListener(this);
 
-        if (mContext.getResources().getBoolean(R.bool.config_audioPanelOnLeftSide))
-            mExpandRows.setDefaultDirection(false);
-
         if (mRows.isEmpty()) {
             if (!AudioSystem.isSingleVolume(mContext)) {
                 addRow(STREAM_ACCESSIBILITY, R.drawable.ic_volume_accessibility,
